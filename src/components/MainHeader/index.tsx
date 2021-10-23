3366f910010fc6ad862cc9aac79e3686ad6ba07b
mateuslopesdecastro <mlcmateus99@gmail.com>
2021-10-23 16:43:43 -0300

    Criação header usuário

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import emojis from '../../utils/emojis';
 
-import { Container } from './styles';
+import {
+    Container,
+    Profile,
+    Welcome,
+    UserName
+} from './styles';
 
-const MainHeader: React.FC = () =>{
+const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random()*emojis.length);
+        return emojis[indice];
+     }, []);
     return (
         <Container>
-        <h1>MainHeader</h1>
+            <h1>Toogle</h1>
+            <Profile>
+                <Welcome>Olá, {emoji} </Welcome>
+                <UserName>Mateus Castro</UserName>
+            </Profile>
         </Container>
     )
 }
