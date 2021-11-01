24ce167bfdf68658d01dbf4532a6ef2a0f4dbccd
mateuslopesdecastro <mlcmateus99@gmail.com>
2021-11-01 20:51:18 -0300

    Criando contexto do tema na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { ThemeProvider } from 'styled-components';
 import Routes from './routes';
 import GlobalStyles from './styles/GlobalStyles';
 
-import dark from './styles/themes/dark';
-import light from './styles/themes/light';
+import { useTheme } from './hooks/theme';
 
 const App: React.FC = () => {
+    const { theme } = useTheme();
     return (
-        <ThemeProvider theme={dark}>
+        <ThemeProvider theme={theme}>
             <GlobalStyles />
-           <Routes />
+            <Routes />
         </ThemeProvider>
     )
 }
