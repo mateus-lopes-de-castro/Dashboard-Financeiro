dc7844a05a36c79384f735366c9fb7fc058ac0e4
mateuslopesdecastro <mlcmateus99@gmail.com>
2021-11-02 13:21:56 -0300

    Criando tela de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Logo, Form, FormTitle } from './styles';
+
 const SignIn: React.FC = () =>{
     return (
-        <h1>SignIn</h1>
+        <Container>
+            <Logo>
+                <img src={logoImg} alt="Dashboard Financeiro" />
+                <h2>Dashboard Financeiro</h2>
+            </Logo>
+
+            <Form>
+                <FormTitle>Entrar</FormTitle>
+                <input type="text"></input>
+                <input type="text"></input>
+
+                <button type="submit">Acessar</button>
+            </Form>
+        </Container>
     )
 }
 
